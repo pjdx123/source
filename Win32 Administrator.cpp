7aed7677132426cbb2d0c234607f20e7586e36ef
pjdx123 <31365202+pjdx123@users.noreply.github.com>
2017-08-26 16:11:22 +0800

    Add files via upload

---
+++ Win32 Administrator.cpp
@@ -0,0 +1,13 @@
+// Win32 Administrator.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("this is first github");
+	getchar();
+	return 0;
+}
+
